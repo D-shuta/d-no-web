55a1af276b1e72a9e7474f375f2f0cd0f91ebcc9
D-shuta <crime-of-visualshock-you-are-x@softbank.ne.jp>
2020-04-13 12:17:29 +0900

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :lps, only: [:index,:new,:create,:show]
   # post 'lps/:id' => 'ips#index'
   resources :puro, only: [:index]
+  resources :supplement, only: [:index]
 end
 
